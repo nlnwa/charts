9ae0f7aa157aa2cbfa518d56c58f05a15c22a204
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-04-04 14:06:55 +0200

    chore: add deploy scripts and certificates used in development

---
+++ scripts/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xe
+
+# Enforce minikube context (not production)
+kubectl config use-context minikube
+
+# Initialize helm on client and server, wait for it to be ready
+helm init --wait
+
+# Add/update helm repo
+helm repo add nlnwa https://nlnwa.github.io/charts
+helm repo update
+
+# Upgrade/install nlnwa/veidemann into default namespace with release name "dev"
+#
+# Add entry to veidemann-controller's /etc/hosts file with minikube's ip pointing to the name
+# of the name of the generated certs
+# See (https://kubernetes.io/docs/concepts/services-networking/add-entries-to-pod-etc-hosts-with-host-aliases/)
+helm upgrade dev nlnwa/veidemann --install --namespace default \
+--set veidemann-controller.config.hostAliases[0].ip=$(minikube ip) \
+--set veidemann-controller.config.hostAliases[0].hostnames[0]=veidemann.local \
+$@
+
