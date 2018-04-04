e259e330b888e23abcc6214cc2d53af9c8a6e957
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-04-04 11:33:29 +0200

    fix: deploy to gh-pages

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-git config user.name "$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
-git config user.email "$(git log -1 $TRAVIS_COMMIT --pretty="%cE")"
-git checkout gh-pages
-#git clone --quiet --branch=gh-pages git@github.com:nlnwa/charts.git /tmp/gh-pages
-#mv *.tgz index.yaml /tmp/gh-pages
-#cd /tmp/gh-pages
-#git add .
-#git commit -fm "chore(travis): $TRAVIS_BUILD_NUMBER"
-#git push -fq origin gh-pages
+# Initialize a new git repo
+git init /tmp/gh-pages
+
+# Move assets into this repo
+mv *.tgz index.yaml /tmp/gh-pages
+
+cd /tmp/gh-pages
+
+git add .
+
+git commit -m "chore(CI): $TRAVIS_BUILD_NUMBER"
+
+git push --force --quiet git@github.com:nlnwa/charts.git master:gh-pages
