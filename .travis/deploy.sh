656e4d253f83b025ca76c6f1da28335864ca7a6b
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-08-24 10:20:31 +0200

    chore(Github Pages): use docs/ folder and not gh-pages branch to publish

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# Initialize a new git repo
-git init /tmp/gh-pages
+set -e
 
-# Move assets into this repo
-mv *.tgz index.yaml /tmp/gh-pages
-
-cd /tmp/gh-pages
+# Note that $TRAVIS_BRANCH is only equal to correct branch if
+# this deploy script is not a pull request or tag build
+git checkout $TRAVIS_BRANCH
 
 git add .
-
-git commit -m "chore(CI): $TRAVIS_BUILD_NUMBER"
-
-git push --force --quiet git@github.com:nlnwa/charts.git master:gh-pages
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
+git remote set-url --push origin git@github.com:${TRAVIS_REPO_SLUG}.git
+git push
