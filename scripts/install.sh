a5faa95859c407fce090b0f10be2b88a484b5125
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-08-24 10:32:13 +0200

    chore: update repo location in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,14 +11,14 @@ helm init --wait
 # Add/update helm repo
 helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
 helm repo update
-helm dep up ../veidemann
+helm dep up ../repo/veidemann
 
 # Upgrade/install nlnwa/veidemann into default namespace with release name "dev"
 #
 # Add entry to veidemann-controller's /etc/hosts file with minikube's ip pointing to the name
 # of the name of the generated certs
 # See (https://kubernetes.io/docs/concepts/services-networking/add-entries-to-pod-etc-hosts-with-host-aliases/)
-helm upgrade dev ../veidemann --install --namespace default \
+helm upgrade dev ../repo/veidemann --install --namespace default \
 --set veidemann-controller.config.hostAliases[0].ip=$(minikube ip) \
 --set veidemann-controller.config.hostAliases[0].hostnames[0]=veidemann.local \
 --set jaeger.cassandra.config.max_heap_size=1024M \
