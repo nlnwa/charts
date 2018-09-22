018edae283be5ba5411a7026ee2f4f3ecbdfaeba
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-09-22 22:05:27 +0200

    refactor: don't rely on bash

--- scripts/hosts.sh
+++ scripts/hosts.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 
 # Append or remove hostname and minikube ip to/from /etc/hosts
 
 HOSTNAME=veidemann.local
+MINIKUBE_IP=$(minikube ip)
 
 # Remove old entries for HOSTNAME in /etc/hosts
 sudo sed -i "/${HOSTNAME}/d" /etc/hosts
 
 # Add entry for HOSTNAME in /etc/hosts pointing to minikube ip
 echo "Adding mapping from veidemann.local to minikube ip to /etc/hosts:"
-echo "$(minikube ip) ${HOSTNAME}" | sudo tee -a /etc/hosts
+echo "${MINIKUBE_IP} ${HOSTNAME}" | sudo tee -a /etc/hosts
